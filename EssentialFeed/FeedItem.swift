bd0556782e500f1e13568db4ade01164473afcd3
Oliver Perez <oliverperez.e@gmail.com>
2020-10-19 23:06:53 -0500

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Oliver Jordy Pérez Escamilla on 19/10/20.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
