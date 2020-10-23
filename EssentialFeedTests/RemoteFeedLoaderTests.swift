6586911025a9c39a32f7098de636f9732dcd3942
Oliver Perez <oliverperez.e@gmail.com>
2020-10-23 00:34:07 -0500

    RemoteFeedLoader does not request data from creation

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,27 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Oliver Jordy Pérez Escamilla on 23/10/20.
+//
+
+import XCTest
+
+final class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+final class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+    
+}
