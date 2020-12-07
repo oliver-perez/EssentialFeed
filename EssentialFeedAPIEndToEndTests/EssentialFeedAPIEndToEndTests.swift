df3e428715607c8cdc4277d6e29e28d67ff23756
Oliver Perez <oliverperez.e@gmail.com>
2020-12-06 18:40:01 -0600

    Add final keyword

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class EssentialFeedAPIEndToEndTests: XCTestCase {
+final class EssentialFeedAPIEndToEndTests: XCTestCase {
 
   func test_endToEndTestServer_matchesFixedTestAccountData() {
     switch getFeedResult() {
