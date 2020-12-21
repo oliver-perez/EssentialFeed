08a86f6f8cce7a6f845e4ad375e8c3c9c7176a40
Oliver Perez <oliverperez.e@gmail.com>
2020-12-20 20:29:08 -0600

    Move `FeedStore` to its own file.

---
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Oliver Jordy Pérez Escamilla on 20/12/20.
+//
+
+import Foundation
+
+public protocol FeedStore {
+  typealias DeletionCompletion = (Error?) -> Void
+  typealias InsertionCompletion = (Error?) -> Void
+  
+  func deleteCachedFeed(completion: @escaping DeletionCompletion)
+  func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
