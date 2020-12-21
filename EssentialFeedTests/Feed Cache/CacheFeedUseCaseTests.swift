c24827511965e00c90ffbe1d60245165329ccd1d
Oliver Perez <oliverperez.e@gmail.com>
2020-12-20 20:21:09 -0600

    Invert if logic to make code paths easier to follow.

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -23,13 +23,13 @@ class LocalFeedLoader {
       
       guard let self = self else { return }
       
-      if error == nil {
+      if let cacheDeletionError = error {
+        completion(cacheDeletionError)
+      } else {
         self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
           guard self != nil else { return }
           completion(error)
         }
-      } else {
-        completion(error)
       }
     }
   }
