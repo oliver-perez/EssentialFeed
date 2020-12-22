a1c0b420846fa4b4e21e44755e2010c96c60db66
Oliver Perez <oliverperez.e@gmail.com>
2020-12-21 23:08:38 -0600

    Move `LocalFeedItem` to its own file.

---
+++ EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,25 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Oliver Jordy Pérez Escamilla on 21/12/20.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID,
+                description: String?,
+                location: String?,
+                imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
