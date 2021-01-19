6943241e35cd2544b57db23afce18a891cdeb648
Oliver Perez <oliverperez.e@gmail.com>
2021-01-18 22:34:45 -0600

    Make the storeURL explicit within the test to facilitate debugging if this test ever fails (all relevant details within a test method should be clearly visible).

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -51,10 +51,13 @@ class CodableFeedStore {
       return completion(.empty)
     }
     
-    let decoder = JSONDecoder()
-    let cache = try! decoder.decode(Cache.self, from: data)
-    completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
-    
+    do {
+      let decoder = JSONDecoder()
+      let cache = try decoder.decode(Cache.self, from: data)
+      completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+    } catch {
+      completion(.failure(error))
+    }
   }
   
   func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
@@ -112,10 +115,19 @@ class CodableFeedStoreTests: XCTestCase {
     expect(sut, toRetrieveTwice: .found(feed: feed, timestamp: timestamp))
   }
   
+  func test_retrieve_deliversFailureOnRetrievalError() {
+    let storeURL = testSpecificStoreURL()
+    let sut = makeSUT(storeURL: storeURL)
+    
+    try! "invalid data".write(to: storeURL, atomically: false, encoding: .utf8)
+    
+    expect(sut, toRetrieve: .failure(anyNSError()))
+  }
+  
   // MARK: - Helpers
   
-  func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
-    let sut = CodableFeedStore(storeURL: testSpecificStoreURL())
+  func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
+    let sut = CodableFeedStore(storeURL: storeURL ?? testSpecificStoreURL())
     trackForMemoryLeaks(sut, file: file, line: line)
     return sut
   }
@@ -141,7 +153,7 @@ class CodableFeedStoreTests: XCTestCase {
     
     sut.retrieve { retrievedResult in
       switch (expectedResult, retrievedResult) {
-      case (.empty, .empty):
+      case (.empty, .empty), (.failure, .failure):
         break
       case let (.found(expected), .found(retrieved)):
         XCTAssertEqual(retrieved.feed, expected.feed, file: file, line: line)
