161afb0b1cd0577c66150248ecdf34f57f2c8eb2
Oliver Perez <oliverperez.e@gmail.com>
2021-01-29 19:37:11 -0600

    Replace custom `HttpClientResult` enum with a wasted type alias over the standard `Swift.Result`

--- EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -155,13 +155,13 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
   }
   
   private final class HTTPClientSpy: HTTPClient {
-    private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+    private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
     
     var requestedURLs: [URL] {
       messages.map { $0.url }
     }
     
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
       messages.append((url, completion))
     }
     
@@ -174,7 +174,7 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
                                      statusCode: code,
                                      httpVersion: nil,
                                      headerFields: nil)!
-      messages[index].completion(.success(data, response))
+      messages[index].completion(.success((data, response)))
     }
   }
   
