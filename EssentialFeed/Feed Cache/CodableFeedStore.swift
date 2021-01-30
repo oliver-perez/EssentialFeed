fc16c28162bdc414ae88dd886d4ba69a6238ae03
Oliver Perez <oliverperez.e@gmail.com>
2021-01-29 20:30:18 -0600

    Replace custom `RetrieveCachedFeedResult` enum with a nested type alias over the standard `Swift.Result` (`FeedStore.RetrievalResult`)

--- EssentialFeed/Feed Cache/CodableFeedStore.swift
+++ EssentialFeed/Feed Cache/CodableFeedStore.swift
@@ -51,13 +51,13 @@ public class CodableFeedStore: FeedStore {
   public func retrieve(completion: @escaping RetrievalCompletion) {
     queue.async { [storeURL] in
       guard let data = try? Data(contentsOf: storeURL) else {
-        return completion(.empty)
+        return completion(.success(.empty))
       }
       
       do {
         let decoder = JSONDecoder()
         let cache = try decoder.decode(Cache.self, from: data)
-        completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+        completion(.success(.found(feed: cache.localFeed, timestamp: cache.timestamp)))
       } catch {
         completion(.failure(error))
       }
