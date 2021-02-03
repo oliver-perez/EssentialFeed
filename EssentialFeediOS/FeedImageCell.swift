eee57813571fc25e0f86afd829815aae058ff3bc
Oliver Perez <oliverperez.e@gmail.com>
2021-02-02 20:34:02 -0600

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Oliver Jordy Pérez Escamilla on 02/02/21.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+  public let locationContainer = UIView()
+  public let locationLabel = UILabel()
+  public let descriptionLabel = UILabel()
+}
