b20252ebbf11b2d725e2a1d0988528efaf7af13e
Oliver Perez <oliverperez.e@gmail.com>
2021-02-05 23:06:01 -0600

    Extract custom assertions to a separate file

--- EssentialFeediOSTests/FeedUI/Controllers/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedUI/Controllers/FeedViewControllerTests.swift
@@ -264,22 +264,6 @@ class FeedViewControllerTests: XCTestCase {
     }
   }
   
-  private func assertThat(_ sut: FeedViewController, hasViewConfiguredFor image: FeedImage, at index: Int, file: StaticString = #filePath, line: UInt = #line) {
-    let view = sut.feedImageView(at: index)
-    
-    guard let cell = view as? FeedImageCell else {
-      return XCTFail("Expected \(FeedImageCell.self) instance, got \(String(describing: view)) instead", file: file, line: line)
-    }
-    
-    let shouldLocationBeVisible = (image.location != nil)
-    XCTAssertEqual(cell.isShowingLocation, shouldLocationBeVisible, "Expected `isShowingLocation` to be \(shouldLocationBeVisible) for the image view at index (\(index))")
-    
-    XCTAssertEqual(cell.locationText, image.location, "Expected location text to be \(String(describing: image.location)) for image view at index (\(index)", file: file, line: line)
-    
-    XCTAssertEqual(cell.descriptionText, image.description, "Expected description text to be \(String(describing: image.description)) for image view at index (\(index)", file: file, line: line)
-    
-  }
-  
   private func makeImage(description: String? = nil, location: String? = nil, url: URL = URL(string: "http://any-url.com")!) -> FeedImage {
     FeedImage(id: UUID(), description: description, location: location, url: url)
   }
---
+++ EssentialFeediOSTests/FeedUI/Controllers/Helpers/FeedViewControllerTests+Assertions.swift
@@ -0,0 +1,31 @@
+//
+//  FeedViewControllerTests+Assertions.swift
+//  EssentialFeediOSTests
+//
+//  Created by Oliver Jordy Pérez Escamilla on 05/02/21.
+//
+
+import XCTest
+import EssentialFeed
+import EssentialFeediOS
+
+extension FeedViewControllerTests {
+  
+  func assertThat(_ sut: FeedViewController, hasViewConfiguredFor image: FeedImage, at index: Int, file: StaticString = #filePath, line: UInt = #line) {
+    let view = sut.feedImageView(at: index)
+    
+    guard let cell = view as? FeedImageCell else {
+      return XCTFail("Expected \(FeedImageCell.self) instance, got \(String(describing: view)) instead", file: file, line: line)
+    }
+    
+    let shouldLocationBeVisible = (image.location != nil)
+    XCTAssertEqual(cell.isShowingLocation, shouldLocationBeVisible, "Expected `isShowingLocation` to be \(shouldLocationBeVisible) for the image view at index (\(index))")
+    
+    XCTAssertEqual(cell.locationText, image.location, "Expected location text to be \(String(describing: image.location)) for image view at index (\(index)", file: file, line: line)
+    
+    XCTAssertEqual(cell.descriptionText, image.description, "Expected description text to be \(String(describing: image.description)) for image view at index (\(index)", file: file, line: line)
+    
+  }
+
+}
+
