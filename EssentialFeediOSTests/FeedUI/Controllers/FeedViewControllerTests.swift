cfeba99fa32d61897b0f766544bdfe7a2d3ed428
Oliver Perez <oliverperez.e@gmail.com>
2021-02-05 22:18:30 -0600

    Extract FeedViewController test helpers to a separate file

--- EssentialFeediOSTests/FeedUI/Controllers/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedUI/Controllers/FeedViewControllerTests.swift
@@ -340,59 +340,6 @@ class FeedViewControllerTests: XCTestCase {
   
 }
 
-private extension FeedViewController {
-  func simulateUserInitiatedFeedReload() {
-    refreshControl?.simulatePullToRefresh()
-  }
-  
-  func simulateFeedImageViewNearVisible(at row: Int) {
-    let ds = tableView.prefetchDataSource
-    let index = IndexPath(row: row, section: feedImagesSection)
-    ds?.tableView(tableView, prefetchRowsAt: [index])
-  }
-  
-  func simulateFeedImageViewNotNearVisible(at row: Int) {
-    simulateFeedImageViewNearVisible(at: row)
-    
-    let ds = tableView.prefetchDataSource
-    let index = IndexPath(row: row, section: feedImagesSection)
-    ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
-  }
-  
-  @discardableResult
-  func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
-    feedImageView(at: index) as? FeedImageCell
-  }
-  
-  func simulateFeedImageViewNotVisible(at row: Int) {
-    let view = simulateFeedImageViewVisible(at: row)
-    
-    let delegate = tableView.delegate
-    let index = IndexPath(row: row, section: feedImagesSection)
-    delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
-  }
-  
-  var isShowingLoadingIndicator: Bool {
-    refreshControl?.isRefreshing == true
-  }
-  
-  func numberOfRendererFeedImageViews() -> Int {
-    tableView.numberOfRows(inSection: feedImagesSection)
-  }
-  
-  func feedImageView(at row: Int) -> UITableViewCell? {
-    let ds = tableView.dataSource
-    let index = IndexPath(row: row, section: feedImagesSection)
-    
-    return ds?.tableView(tableView, cellForRowAt: index)
-  }
-  
-  private var feedImagesSection: Int {
-    0
-  }
-  
-}
-
 private extension FeedImageCell {
   
   func simulateRetryAction() {
