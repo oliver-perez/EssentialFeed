384c10b8ad340a0ce7948c4d3e62168ff59ed9d3
Oliver Perez <oliverperez.e@gmail.com>
2021-02-07 19:31:06 -0600

    Move `FeedLoader` loading state management to `FeedViewModel`. Now, the `FeedRefreshViewController` acts as a dinner between the `View` and the `ViewModel`

--- EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -6,31 +6,32 @@
 //
 
 import UIKit
-import EssentialFeed
 
 final class FeedRefreshViewController: NSObject {
-  private(set) lazy var view: UIRefreshControl = {
-    let view = UIRefreshControl()
-    view.addTarget(self, action: #selector(refresh), for: .valueChanged)
-    return view
-  }()
   
-  private let feedLoader: FeedLoader
+  private(set) lazy var view: UIRefreshControl = binded(UIRefreshControl())
   
-  init(feedLoader: FeedLoader) {
-    self.feedLoader = feedLoader
+  private let viewModel: FeedViewModel
+  
+  init(viewModel: FeedViewModel) {
+    self.viewModel = viewModel
+  }
+    
+  @objc func refresh() {
+    viewModel.loadFeed()
   }
   
-  var onRefresh: (([FeedImage]) -> Void)?
-  
-  @objc func refresh() {
-    view.beginRefreshing()
-    feedLoader.load { [weak self] result in
-      if let feed = try? result.get() {
-        self?.onRefresh?(feed)
+  private func binded(_ view: UIRefreshControl) -> UIRefreshControl {
+    viewModel.onChange = { [weak self] viewModel in
+      if viewModel.isLoading {
+        self?.view.beginRefreshing()
+      } else {
+        self?.view.endRefreshing()
       }
-      self?.view.endRefreshing()
+
     }
+    view.addTarget(self, action: #selector(refresh), for: .valueChanged)
+    return view
   }
   
 }
