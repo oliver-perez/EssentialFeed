66421f2655763c6cc385b4a71a5b6568fc216a6c
Oliver Perez <oliverperez.e@gmail.com>
2021-02-09 20:48:19 -0600

    Move ViewModels to separate files

---
+++ EssentialFeediOS/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Oliver Jordy Pérez Escamilla on 09/02/21.
+//
+
+struct FeedLoadingViewModel {
+  let isLoading: Bool
+}
---
+++ EssentialFeediOS/FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Oliver Jordy Pérez Escamilla on 09/02/21.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+  let feed: [FeedImage]
+}
