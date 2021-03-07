4174cbf1116caba09e10ee5f315c9e3f8876bbdc
Oliver Perez <oliverperez.e@gmail.com>
2021-03-06 22:09:58 -0600

    Load `NSManagedObjectModel` instance lazily and cache it to prevent m…

--- EssentialFeed/EssentialFeed/Feed Cache/FeedImageDataStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedImageDataStore.swift
@@ -1,18 +1,13 @@
 //
-//  FeedImageDataStore.swift
-//  EssentialFeed
-//
-//  Created by Oliver Jordy Pérez Escamilla on 05/03/21.
-//  Copyright © 2021 Essential Developer. All rights reserved.
+//  Copyright © 2019 Essential Developer. All rights reserved.
 //
 
 import Foundation
 
 public protocol FeedImageDataStore {
-  typealias RetrievalResult = Swift.Result<Data?, Error>
-  typealias InsertionResult = Swift.Result<Void, Error>
+	typealias RetrievalResult = Swift.Result<Data?, Error>
+	typealias InsertionResult = Swift.Result<Void, Error>
 
-  func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void)
-  func retrieve(dataForURL url: URL, completion: @escaping (RetrievalResult) -> Void)
+	func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void)
+	func retrieve(dataForURL url: URL, completion: @escaping (RetrievalResult) -> Void)
 }
-
